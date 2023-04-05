5b2df1e9e6d54298607b75151b761f6a25aa0cc8
yamaguchi <yamaguchi.takahiro.52c@gmail.com>
2023-04-05 15:57:01 +0900

    syokai

--- myprj/settings.py
+++ myprj/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import decouple import config
+from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
